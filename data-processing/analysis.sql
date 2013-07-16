75352a458cac863feab05c5a866ddedf6aa4f109
Alan Palazzolo <apalazzolo@minnpost.com>
2013-07-16 17:30:07 -0500

    Updating SQL analysis.

--- data-processing/analysis.sql
+++ data-processing/analysis.sql
@@ -1,36 +1,80 @@
 -- Create (and drop) analysis table
-DROP TABLE analysis; CREATE TABLE analysis 
+DROP TABLE IF EXISTS analysis; 
+
+CREATE TABLE analysis 
 SELECT 
-  noncritical.`license address`, 
-  noncritical.`name of business`, 
-  critical.`count` AS critical_count,
-  noncritical.`count` AS noncritical_count,
-  (critical.`count` + noncritical.`count`) AS total_violations,
-  (critical.`count` / (critical.`count` + noncritical.`count`) * 100) AS critical_percentage
+  business.id,
+  business.`license address` AS address, 
+  business.`name of business` AS business,
+  business.total_violations AS total_violations,
+  inspection_count.count AS inspection_count,
+  IF(critical_count.count IS NULL, 0, critical_count.count) AS critical_count,
+  IF(noncritical_count.count IS NULL, 0, noncritical_count.count) AS noncritical_count
 FROM 
-  (SELECT `license address`, `name of business`, COUNT(`critical`) AS count 
-    FROM critical GROUP BY `license address`, `name of business`) AS critical,
-  (SELECT `license address`, `name of business`, COUNT(`critical`) AS count 
-    FROM noncritical GROUP BY `license address`, `name of business`) AS noncritical
-WHERE
-  critical.`license address` = noncritical.`license address`
-  AND critical.`name of business` = noncritical.`name of business`
-ORDER BY
-  critical_percentage DESC,
-  critical_count DESC
+  (SELECT CONCAT(`license address`, `name of business`) AS id,
+    `license address`, `name of business`, COUNT(*) AS total_violations
+    FROM `InspectionHistory-008`
+    GROUP BY `license address`, `name of business`) AS business
+  LEFT JOIN
+    (SELECT CONCAT(`license address`, `name of business`) AS id,
+      COUNT(DISTINCT `InspectionID`) AS count
+      FROM `InspectionHistory-008`
+      GROUP BY `license address`, `name of business`) AS inspection_count
+    ON inspection_count.id = business.id
+  LEFT JOIN
+    (SELECT CONCAT(`license address`, `name of business`) AS id,
+      COUNT(`critical`) AS count
+      FROM `InspectionHistory-008`
+      WHERE `critical` = 'Yes'
+      GROUP BY `license address`, `name of business`) AS critical_count
+    ON critical_count.id = business.id
+  LEFT JOIN
+    (SELECT CONCAT(`license address`, `name of business`) AS id,
+      COUNT(`critical`) AS count
+      FROM `InspectionHistory-008`
+      WHERE `critical` <> 'Yes'
+      GROUP BY `license address`, `name of business`) AS noncritical_count
+    ON noncritical_count.id = business.id
+;
+-- Add some other columns
+ALTER TABLE analysis ADD critical_violation_ratio FLOAT;
+UPDATE analysis SET critical_violation_ratio = (critical_count / total_violations);
+
+ALTER TABLE analysis ADD violation_inspection_ratio FLOAT;
+UPDATE analysis SET violation_inspection_ratio = (total_violations / inspection_count);
+
+ALTER TABLE analysis ADD critical_inspection_ratio FLOAT;
+UPDATE analysis SET critical_inspection_ratio = (critical_count / inspection_count);
   
 
 -- Averages
--- total: 15.68822023
--- percent: 30.68050602
 SELECT
-  'Average total violations',
-  AVG(total_violations)
-FROM
-  analysis
+  'Average total violations' AS label,
+  AVG(total_violations) AS stat
+FROM analysis
 UNION
 SELECT
-  'Average percentage is critical',
-  AVG(critical_percentage)
-FROM
-  analysis
+  'Average inspections' AS label,
+  AVG(inspection_count) AS stat
+FROM analysis
+UNION
+SELECT
+  'Average critical violations' AS label,
+  AVG(critical_count) AS stat
+FROM analysis
+UNION
+SELECT
+  'Average critical to violation ratio' AS label,
+  AVG(critical_violation_ratio) AS stat
+FROM analysis
+UNION
+SELECT
+  'Average violations to inspections ratio' AS label,
+  AVG(violation_inspection_ratio) AS stat
+FROM analysis
+UNION
+SELECT
+  'Average critical violations to inspections ratio' AS label,
+  AVG(critical_inspection_ratio) AS stat
+FROM analysis
+;
